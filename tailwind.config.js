261b338cd66a6d33645bfc426446686cb2c99242
Emillyalmeida <emillyalmeidan@gmail.com>
2024-02-10 22:49:34 -0300

    feat: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{tsx,ts}", "./src/components/**/*.{tsx,ts}"
+  ],
   theme: {
     extend: {},
   },
