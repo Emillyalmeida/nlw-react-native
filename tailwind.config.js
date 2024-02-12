6e52aabd39892a8072a9c0f8bc3ca24e4443a4fb
Emillyalmeida <emillyalmeidan@gmail.com>
2024-02-11 23:14:34 -0300

    feat: add loading and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/app/**/*.{tsx,ts}", "./src/components/**/*.{tsx,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        bold: "Inter_700Bold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular"
+      }
+    },
   },
   plugins: [],
 }
