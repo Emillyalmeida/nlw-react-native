389d445989930717ea5336f6aff475960311f16c
Emillyalmeida <emillyalmeidan@gmail.com>
2024-02-11 23:15:50 -0300

    feat: create compenents category and header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,31 @@
-import { View, Text} from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import { useState } from 'react'
+import { Header } from '../components/header'
+import { CategoryItem } from '../components/categoryItem'
+import { CATEGORIES } from '../utils/data/products'
+
 
 export default function Home() {
+    const [ category, setCategory ] = useState(CATEGORIES[0])
+
+    function handerSelectCategory (selectedCategory : string){
+        setCategory(selectedCategory)
+    }
+
     return (
         <View className="flex-1 pt-8">
-            <Text className="text-white text-2xl">Hello World</Text>
+            <Header title='Cardapio'/>
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item)=> item}
+                renderItem={({item})=> 
+                    <CategoryItem title={ item } isSelected={ item === category } onPress={ () => handerSelectCategory(item) }/>
+                }
+                horizontal
+                className='max-h-10 mt-5'
+                showsHorizontalScrollIndicator={ false }
+                contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+            />
         </View>
     )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import { Image, View, TouchableOpacity, Text } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
+
+type headerProps = {
+    title: string,
+    cartItens?: number
+}
+
+export function Header({ title, cartItens = 0  }: headerProps) {
+    return (
+        <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5 mt-3">
+            <View className="flex-1">
+                <Image source={require("@/src/assets/logo.png")} className="h-6 w-32"/>
+                <Text className="text-white text-xl font-headind mt-3">{ title }</Text>
+            </View>
+            
+            <TouchableOpacity className="relative ml-4">
+                <View className="bg-lime-300 h-4 w-4 rounded-full items-center justify-center absolute -top-2 z-10 -right-1">
+                    <Text className="text-slate-900 font-bold text-xs">{ cartItens }</Text>
+                </View>
+                <Feather name="shopping-bag" color={colors.white} size={24}/>
+            </TouchableOpacity>
+        </View>
+    )
+}
